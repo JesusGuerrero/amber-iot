21a8ec73641d064b8495d057258c43fe14db1ec6
Amber Slovick <amber.slovick@gmail.com>
2016-11-04 13:08:50 -0700

    testing power control

--- midterm/button-screen.js
+++ midterm/button-screen.js
@@ -1,15 +1,15 @@
 var gpio = require('onoff').Gpio,
   button = new gpio(17, 'in', 'both'),
-  powercontrol = new gpio(26, 'out'),
-  powerstate = 0;
+  powercontrol = new gpio(26, 'out');
+  //powerstate = 0;
 
 button.setActiveLow( true );	
 
 button.watch(function(err, value) {	
   console.log('Button is ' + (value ? 'ON' : 'OFF'));
 
-  powercontrol.writeSync( powerstate );
-  powerstate = powerstate ? 0 : 1;
+  powercontrol.writeSync( value ? 1 : 0 );
+  //powerstate = powerstate ? 0 : 1;
 
   console.log('Screen is ' + (value ? 'ON' : 'OFF'));
 });

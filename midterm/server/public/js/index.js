b5a81b5cb3bb0d9bdc9f06ad6a75983fd1c3ba7c
Amber Slovick <amber.slovick@gmail.com>
2016-11-03 20:25:34 -0700

    testing connection between camera and server

--- midterm/server/public/js/index.js
+++ midterm/server/public/js/index.js
@@ -1,1 +1,15 @@
-alert('hello world');
+var socket = io();
+
+socket.on('event:hello', function(){
+  console.log('Hello from server through socket');
+});
+
+angular.module('fridge-client', [])
+  .controller('MainCtrl', ['$scope', function( $scope ) {
+    $scope.camera = false;
+    socket.on('event:camera', function( val ) {
+      $scope.$apply( function() {
+        $scope.camera = val ? true : false;
+      });
+    });
+  }]);

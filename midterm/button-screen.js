ef88ba6d2e673c0ae7e552f7f3ee5529208223fd
Amber Slovick <amber.slovick@gmail.com>
2016-11-03 18:28:51 -0700

    testing usb power control with button

---
+++ midterm/button-screen.js
@@ -0,0 +1,17 @@
+var gpio = require('onoff').Gpio,
+  button = new Gpio(17, 'in', 'both'),
+  powercontrol = new gpio(26, 'out');
+
+button.setActiveLow( true );		
+
+button.watch(function(err, value) {	
+  console.log('Button is ' + (value ? 'ON' : 'OFF'));
+
+  powercontrol.writeSync(val);
+});
+
+process.on('SIGINT', function(){
+  button.unexport();
+  powercontrol.unexport();
+  process.exit();
+}); 

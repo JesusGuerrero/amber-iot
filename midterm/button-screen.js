417aa1b4d248822ff01a6671c6841bf15cef85e8
Amber Slovick <amber.slovick@gmail.com>
2016-11-18 12:31:22 -0800

    change GPIO pin

--- midterm/button-screen.js
+++ midterm/button-screen.js
@@ -1,7 +1,6 @@
 var gpio = require('onoff').Gpio,
   button = new gpio(17, 'in', 'both'),
-  powercontrol = new gpio(26, 'out');
-  //powerstate = 0;
+  usb = new gpio(5, 'high', 'rising', {activeLow: true} ); 
 
 button.setActiveLow( true );	
 
@@ -9,7 +8,6 @@ button.watch(function(err, value) {
   console.log('Button is ' + (value ? 'ON' : 'OFF'));
 
   powercontrol.writeSync( value ? 1 : 0 );
-  //powerstate = powerstate ? 0 : 1;
 
   console.log('Screen is ' + (value ? 'ON' : 'OFF'));
 });

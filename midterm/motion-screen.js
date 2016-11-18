28b216db159fb4ffa6a7d62948b0a9fbb7a4e44c
Amber Slovick <amber.slovick@gmail.com>
2016-11-18 13:06:34 -0800

    troubleshooting

--- midterm/motion-screen.js
+++ midterm/motion-screen.js
@@ -1,12 +1,11 @@
 var gpio = require('onoff').Gpio,
   motion = new gpio(21, 'in', 'both'),
-  usb = new gpio(5, 'high', 'rising'); 
+  usb = new gpio(5, 'high', 'rising', {activeLow: true} ); 
 
 motion.watch( function( err, val ) {
   if( err ) { console.log('Motion Error'); return; }
   console.log('Motion in 21: ' + (val ? 'ACTIVE' : 'INACTIVE') + ' ' + new Date().toLocaleString() );
-  usb.writeSync( val ? 0 : 1 );
-
+  usb.writeSync( val ? 1 : 0 );
 });
 
 process.on('SIGINT', function(){

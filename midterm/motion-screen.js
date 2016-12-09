20271faac4370ae31a40df3c62e7a97ddf6708aa
Amber Slovick <amber.slovick@gmail.com>
2016-12-09 13:23:36 -0800

    added motion timer and changed folder name

--- midterm/motion-screen.js
+++ midterm/motion-screen.js
@@ -1,11 +1,27 @@
 var gpio = require('onoff').Gpio,
   motion = new gpio(21, 'in', 'both'),
-  usb = new gpio(5, 'high', 'rising', {activeLow: true} ); 
+  usb = new gpio(5, 'high', 'rising', {activeLow: true} ),
+  //usbState = 0,
+  countdown; 
 
 motion.watch( function( err, val ) {
-  if( err ) { console.log('Motion Error'); return; }
+  if( err ) { console.log('Motion Error'); return; };
+
+  if (motion.readSync() == 0 & usb.readSync() == 0) {
+  	countdown = setTimeout(function(){
+  		usb.writeSync(0);
+  		//usbState = 0;
+  	}, 30000);
+  };
+
+  if (motion.readSync() == 1) {
+  	clearTimeout(countdown);
+  	usb.writeSync(1);
+  	//usbState = 1;
+  };
+
   console.log('Motion in 21: ' + (val ? 'ACTIVE' : 'INACTIVE') + ' ' + new Date().toLocaleString() );
-  usb.writeSync( val ? 1 : 0 );
+  //usb.writeSync( val ? 1 : 0 );
 });
 
 process.on('SIGINT', function(){

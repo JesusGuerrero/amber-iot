f83380cd88a9fb4f974c2f825ce8bc674bc9d96d
Amber Slovick <amber.slovick@gmail.com>
2016-12-09 13:30:25 -0800

    edits based on instructor feedback

--- midterm/motion-screen.js
+++ midterm/motion-screen.js
@@ -1,27 +1,26 @@
 var gpio = require('onoff').Gpio,
   motion = new gpio(21, 'in', 'both'),
   usb = new gpio(5, 'high', 'rising', {activeLow: true} ),
-  //usbState = 0,
-  countdown; 
+  countdown = null; 
 
 motion.watch( function( err, val ) {
   if( err ) { console.log('Motion Error'); return; };
 
-  if (motion.readSync() == 0 & usb.readSync() == 0) {
+  if (val == 0 & usb.readSync() == 1) {
   	countdown = setTimeout(function(){
   		usb.writeSync(0);
-  		//usbState = 0;
-  	}, 30000);
+  	}, 2000);
   };
 
-  if (motion.readSync() == 1) {
-  	clearTimeout(countdown);
+  if (val == 1) {
+  	if (countdown) {
+  		clearTimeout(countdown);
+  		countdown = null;
+  	}
   	usb.writeSync(1);
-  	//usbState = 1;
   };
 
   console.log('Motion in 21: ' + (val ? 'ACTIVE' : 'INACTIVE') + ' ' + new Date().toLocaleString() );
-  //usb.writeSync( val ? 1 : 0 );
 });
 
 process.on('SIGINT', function(){
